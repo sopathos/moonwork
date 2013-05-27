2b4c3bcdf6af8aa4e42aa09da8ecfd9a1eb221b7
neopjm109 <neopjm109@gmail.com>
2013-05-27 12:27:23 +0900

    Shop and Theme are modified that size down

--- shop.php
+++ shop.php
@@ -9,6 +9,7 @@
   <?
     $shopList = mysql_query("select * from shop order by name asc", $conn);
     $shopListRows = mysql_num_rows($shopList);
+    $shopId;
   ?>
   
   <!-- 내용 -->
@@ -56,53 +57,7 @@
       <tr>
         <td width="50%" align="center"><img border="0" width="85%" src="image/Yoongane.png"></td>
         <td width="50%" align="center">
-          <table>
-            <tr>
-              <th align="center" height="30"><h2><? echo mysql_result($shop, $j, name); ?></h2></th>
-            </tr>
-            <tr><td align="center" height="30"><? echo mysql_result($shop, $j, phone); ?></td></tr>
-            <tr><td align="center" height="30">
-            <?
-              $iter=0;
-              for($iter; $iter<round($data[score]); $iter++){
-                echo "★";
-              }
-              for($iter ; $iter<5 ; $iter++){
-                echo "☆";
-              }
-              echo "(".mysql_result($shop, $j, score)."/5)"; ?>
-            </td></tr>
-            <tr>
-              <td align="center" height="30">
-            <?
-              if($data[deliver]==1){
-                echo "배달 가능";
-              }else{
-                echo "배달 불가";
-              }
-            ?>
-              </td>
-            </tr>
-          </table>
-        </td>
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="50%">음식 이름</td><td align="center" width="50%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='50%' align='center'>".mysql_result($food, $i, food_name)."</td>";
-                echo "<td align='center' width='50%'>".mysql_result($food, $i, price)."</td></tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($food);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
+    <? include ("shop_content.php"); ?>
   </div>
     
   <!-- 푸터 -->
@@ -133,53 +88,7 @@
       <tr>
         <td width="50%" align="center"><img border="0" width="85%" src="image/none.png"></td>
         <td width="50%" align="center">
-          <table>
-            <tr>
-              <th align="center" height="30"><h2><? echo mysql_result($shop, $j, name); ?></h2></th>
-            </tr>
-            <tr><td align="center" height="30"><? echo mysql_result($shop, $j, phone); ?></td></tr>
-            <tr><td align="center" height="30">
-            <?
-              $iter=0;
-              for($iter; $iter<round($data[score]); $iter++){
-                echo "★";
-              }
-              for($iter ; $iter<5 ; $iter++){
-                echo "☆";
-              }
-              echo "(".mysql_result($shop, $j, score)."/5)"; ?>
-            </td></tr>
-            <tr>
-              <td align="center" height="30">
-            <?
-              if($data[deliver]==1){
-                echo "배달 가능";
-              }else{
-                echo "배달 불가";
-              }
-            ?>
-              </td>
-            </tr>
-          </table>
-        </td>
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="50%">음식 이름</td><td align="center" width="50%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='50%' align='center'>".mysql_result($food, $i, food_name)."</td>";
-                echo "<td align='center' width='50%'>".mysql_result($food, $i, price)."</td></tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($food);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
+    <? include ("shop_content.php"); ?>
   </div>
   
   <!-- 푸터 -->
@@ -210,53 +119,7 @@
       <tr>
         <td width="50%" align="center"><img border="0" width="85%" src="image/none.png"></td>
         <td width="50%" align="center">
-          <table>
-            <tr>
-              <th align="center" height="30"><h2><? echo mysql_result($shop, $j, name); ?></h2></th>
-            </tr>
-            <tr><td align="center" height="30"><? echo mysql_result($shop, $j, phone); ?></td></tr>
-            <tr><td align="center" height="30">
-            <?
-              $iter=0;
-              for($iter; $iter<round($data[score]); $iter++){
-                echo "★";
-              }
-              for($iter ; $iter<5 ; $iter++){
-                echo "☆";
-              }
-              echo "(".mysql_result($shop, $j, score)."/5)"; ?>
-            </td></tr>
-            <tr>
-              <td align="center" height="30">
-            <?
-              if($data[deliver]==1){
-                echo "배달 가능";
-              }else{
-                echo "배달 불가";
-              }
-            ?>
-              </td>
-            </tr>
-          </table>
-        </td>
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="50%">음식 이름</td><td align="center" width="50%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='50%' align='center'>".mysql_result($food, $i, food_name)."</td>";
-                echo "<td align='center' width='50%'>".mysql_result($food, $i, price)."</td></tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($food);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
+    <? include ("shop_content.php"); ?>
   </div>
   
   <!-- 푸터 -->
@@ -287,53 +150,7 @@
       <tr>
         <td width="50%" align="center"><img border="0" width="85%" src="image/none.png"></td>
         <td width="50%" align="center">
-          <table>
-            <tr>
-              <th align="center" height="30"><h2><? echo mysql_result($shop, $j, name); ?></h2></th>
-            </tr>
-            <tr><td align="center" height="30"><? echo mysql_result($shop, $j, phone); ?></td></tr>
-            <tr><td align="center" height="30">
-            <?
-              $iter=0;
-              for($iter; $iter<round($data[score]); $iter++){
-                echo "★";
-              }
-              for($iter ; $iter<5 ; $iter++){
-                echo "☆";
-              }
-              echo "(".mysql_result($shop, $j, score)."/5)"; ?>
-            </td></tr>
-            <tr>
-              <td align="center" height="30">
-            <?
-              if($data[deliver]==1){
-                echo "배달 가능";
-              }else{
-                echo "배달 불가";
-              }
-            ?>
-              </td>
-            </tr>
-          </table>
-        </td>
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="50%">음식 이름</td><td align="center" width="50%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='50%' align='center'>".mysql_result($food, $i, food_name)."</td>";
-                echo "<td align='center' width='50%'>".mysql_result($food, $i, price)."</td></tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($food);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
+    <? include ("shop_content.php"); ?>
   </div>
   
   <!-- 푸터 -->
@@ -364,53 +181,7 @@
       <tr>
         <td width="50%" align="center"><img border="0" width="85%" src="image/none.png"></td>
         <td width="50%" align="center">
-          <table>
-            <tr>
-              <th align="center" height="30"><h2><? echo mysql_result($shop, $j, name); ?></h2></th>
-            </tr>
-            <tr><td align="center" height="30"><? echo mysql_result($shop, $j, phone); ?></td></tr>
-            <tr><td align="center" height="30">
-            <?
-              $iter=0;
-              for($iter; $iter<round($data[score]); $iter++){
-                echo "★";
-              }
-              for($iter ; $iter<5 ; $iter++){
-                echo "☆";
-              }
-              echo "(".mysql_result($shop, $j, score)."/5)"; ?>
-            </td></tr>
-            <tr>
-              <td align="center" height="30">
-            <?
-              if($data[deliver]==1){
-                echo "배달 가능";
-              }else{
-                echo "배달 불가";
-              }
-            ?>
-              </td>
-            </tr>
-          </table>
-        </td>
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="50%">음식 이름</td><td align="center" width="50%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='50%' align='center'>".mysql_result($food, $i, food_name)."</td>";
-                echo "<td align='center' width='50%'>".mysql_result($food, $i, price)."</td></tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($food);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
+    <? include ("shop_content.php"); ?>
   </div>
   
   <!-- 푸터 -->
@@ -441,53 +212,7 @@
       <tr>
         <td width="50%" align="center"><img border="0" width="85%" src="image/none.png"></td>
         <td width="50%" align="center">
-          <table>
-            <tr>
-              <th align="center" height="30"><h2><? echo mysql_result($shop, $j, name); ?></h2></th>
-            </tr>
-            <tr><td align="center" height="30"><? echo mysql_result($shop, $j, phone); ?></td></tr>
-            <tr><td align="center" height="30">
-            <?
-              $iter=0;
-              for($iter; $iter<round($data[score]); $iter++){
-                echo "★";
-              }
-              for($iter ; $iter<5 ; $iter++){
-                echo "☆";
-              }
-              echo "(".mysql_result($shop, $j, score)."/5)"; ?>
-            </td></tr>
-            <tr>
-              <td align="center" height="30">
-            <?
-              if($data[deliver]==1){
-                echo "배달 가능";
-              }else{
-                echo "배달 불가";
-              }
-            ?>
-              </td>
-            </tr>
-          </table>
-        </td>
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="50%">음식 이름</td><td align="center" width="50%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='50%' align='center'>".mysql_result($food, $i, food_name)."</td>";
-                echo "<td align='center' width='50%'>".mysql_result($food, $i, price)."</td></tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($food);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
+    <? include ("shop_content.php"); ?>
   </div>
   
   <!-- 푸터 -->
--- theme.php
+++ theme.php
@@ -46,29 +46,7 @@
   ?>
   
   <!-- 내용 -->
-  <div data-role="content">
-    <table align="center" width="100%">
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="30%">음식 이름</td><td align="center" width="40%">파는 곳</td><td align="center" width="30%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodListRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='30%' align='center'>".mysql_result($foodList, $i, food.".".name)."</td>";
-                echo "<td align='center' width='40%'>".mysql_result($foodList, $i, menu.".".shop_name)."</td>";
-                echo "<td align='center' width='30%'>".mysql_result($foodList, $i, menu.".".price)."</td>";
-                echo "</tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($foodList);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
-  </div>
+  <? include("theme_content.php") ?>
     
   <!-- 푸터 -->
   <?
@@ -88,29 +66,7 @@
   ?>
   
   <!-- 내용 -->
-  <div data-role="content">
-    <table align="center" width="100%">
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="30%">음식 이름</td><td align="center" width="40%">파는 곳</td><td align="center" width="30%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodListRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='30%' align='center'>".mysql_result($foodList, $i, food.".".name)."</td>";
-                echo "<td align='center' width='40%'>".mysql_result($foodList, $i, menu.".".shop_name)."</td>";
-                echo "<td align='center' width='30%'>".mysql_result($foodList, $i, menu.".".price)."</td>";
-                echo "</tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($foodList);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
-  </div>
+  <? include("theme_content.php") ?>
     
   <!-- 푸터 -->
   <?
@@ -130,29 +86,7 @@
   ?>
   
   <!-- 내용 -->
-  <div data-role="content">
-    <table align="center" width="100%">
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="30%">음식 이름</td><td align="center" width="40%">파는 곳</td><td align="center" width="30%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodListRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='30%' align='center'>".mysql_result($foodList, $i, food.".".name)."</td>";
-                echo "<td align='center' width='40%'>".mysql_result($foodList, $i, menu.".".shop_name)."</td>";
-                echo "<td align='center' width='30%'>".mysql_result($foodList, $i, menu.".".price)."</td>";
-                echo "</tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($foodList);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
-  </div>
+  <? include("theme_content.php") ?>
     
   <!-- 푸터 -->
   <?
@@ -172,29 +106,7 @@
   ?>
   
   <!-- 내용 -->
-  <div data-role="content">
-    <table align="center" width="100%">
-      <tr>
-        <td colspan="2">
-          <ul data-role="listview" data-inset="true">
-            <li data-theme="a"><table width="100%"><tr><td align="center" width="30%">음식 이름</td><td align="center" width="40%">파는 곳</td><td align="center" width="30%">가격</td></table></li>
-            <?
-              $i = 0;
-              while($i < $foodListRow){
-                echo "<li><table width='100%'><tr>";
-                echo "<td width='30%' align='center'>".mysql_result($foodList, $i, food.".".name)."</td>";
-                echo "<td align='center' width='40%'>".mysql_result($foodList, $i, menu.".".shop_name)."</td>";
-                echo "<td align='center' width='30%'>".mysql_result($foodList, $i, menu.".".price)."</td>";
-                echo "</tr></table></li>";
-                $i++;
-              }
-              mysql_free_result($foodList);
-            ?>
-          </ul>
-        </td>
-      </tr>
-    </table>
-  </div>
+  <? include("theme_content.php") ?>
     
   <!-- 푸터 -->
   <?
